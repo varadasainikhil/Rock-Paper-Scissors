9cb074aba68d98d3ca2f1516e636f21416fcbeb7
varadasainikhil <varadasainikhil@gmail.com>
2024-05-26 10:22:53 -0400

    Initial Commit

---
+++ Rock Paper Scissors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rock Paper Scissors
+//
+//  Created by Sai Nikhil Varada on 5/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
